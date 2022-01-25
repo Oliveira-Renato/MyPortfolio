98cbbff3cab1260497f0a08b1082b99132ba96a9
Renato Oliveira <renato_fortuna@outlook.com>
2022-01-25 17:02:49 -0300

    Projects going dark

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -3,19 +3,24 @@ import $ from 'jquery';
 import { useState, useEffect } from 'react';
 
 export function Projects(){
-    useEffect(()=>{
+  const [item, setItem] = useState('');
+  var nextBtn: any= document.querySelector('.next');
+
+   useEffect(()=>{
     var carousel: any =$(".carousel"),currdeg  = 0;
 
-    $(".next").on("click", { d: "n" }, rotate);
-    $(".prev").on("click", { d: "p" }, rotate);
+    // $(".next").on("click", { d: "n" }, rotate);
+    // $(".prev").on("click", { d: "p" }, rotate);
 
     function rotate(e: any){
+      console.log(e)
       if(e.data.d=="n"){
         currdeg = currdeg - 60;
       }
       
       if(e.data.d=="p"){
         currdeg = currdeg + 60;
+
       }
 
       carousel.css({
@@ -25,7 +30,12 @@ export function Projects(){
         "transform": "rotateY("+currdeg+"deg)"
       });
     }
-  })
+
+    nextBtn.addEventListener('click',function(){
+      console.log('salve')
+    })
+   })
+
 
   return (
     <section className="project" id="project">
@@ -39,10 +49,12 @@ export function Projects(){
               <div className="item b">B</div>
               <div className="item c">C</div>
               <div className="item d">D</div>
+              <div className="item e">E</div>
+              <div className="item f">F</div>
           </div>
         </div>
         <div className="next">Next</div>
-        <div className="prev">Prev</div>
+        <div  className="prev">Prev</div>
     </section>
-  )
+  )//rotate({d:'n'})
 }
